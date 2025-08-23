efacf69faaf870146c192bf8746ac1b7d99b4f1f
KARAN-SHARXA <karansharma2302578@gmail.com>
2025-08-23 13:40:40 +0530

    timers

---
+++ 13timer.js
@@ -0,0 +1,14 @@
+// 1️⃣ setTimeout()
+
+// Ek function ko ek specific delay ke baad run karta hai (1 time)
+
+
+console.log("start");
+
+setTimeout(() => {
+    console.log("this run after 2 second");
+    
+    
+}, 2000);
+console.log("end");
+
